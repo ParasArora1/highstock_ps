614e9a71da988e86eea047fda457ec6f4e289503
ParasArora1 <76914322+ParasArora1@users.noreply.github.com>
2025-01-21 17:57:00 +0530

    Update NewUser.tsx

--- h_p_c/src/pages/NewUser.tsx
+++ h_p_c/src/pages/NewUser.tsx
@@ -40,7 +40,7 @@ const FormComponent = () => {
     setError('');
 
     try {
-      const response = await fetch('https://wild-boxes-trade.loca.lt/users', {
+      const response = await fetch(' https://purple-goats-go.loca.lt/users', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
