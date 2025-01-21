ccd77994bed01eefe85d1ed34b217ca33830fa17
ParasArora1 <76914322+ParasArora1@users.noreply.github.com>
2025-01-21 12:01:55 +0530

    Update ManagePlayers.tsx

--- h_p_c/src/pages/ManagePlayers.tsx
+++ h_p_c/src/pages/ManagePlayers.tsx
@@ -31,7 +31,7 @@ interface UserPizzaHistory {
   eaten_at: string | null;
 }
 
-const API_URL = "https://highstock-ps-1.onrender.com";
+const API_URL = "http://192.168.29.1:5000";
 
 const ManagePlayers: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
